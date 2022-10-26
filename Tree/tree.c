eeb906d8c322f60b5c813e4c815c1cc3a291fabc
simminseo <1350sms123@gmail.com>
2022-10-26 19:26:09 +0900

    new

--- Tree/tree.c
+++ Tree/tree.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <malloc.h>
 #include <stdlib.h>
 #include <string.h>
 #include <search.h>
@@ -17,10 +16,11 @@ int compare(const void *cp1, const void *cp2)
 {
     return strcmp(((struct student *)cp1)->name, ((struct student *)cp2)->name);
 }
+
 void print_node(const void *arr, VISIT order, int level)
 {
     if (order == preorder || order == leaf)
-        printf("이름 = %s, 중간시험 점수 = %d, 기말시험 점수 = %d\n",
+        printf("name = %s, middle = %d, final = %d\n",
                (*(struct student **)arr)->name,
                (*(struct student **)arr)->mid,
                (*(struct student **)arr)->final);
@@ -28,38 +28,31 @@ void print_node(const void *arr, VISIT order, int level)
 
 int main()
 {
-    struct student **ref;
     int num;
-    printf("학생수 입력 : ");
+    int i = 0;
+    printf("입력할 학생 수 : ");
     scanf("%d", &num);
+    char *p_name = (char *)malloc(sizeof(char) * (num * 30));
+    struct student *tree = (struct student *)malloc(sizeof(struct student) * num);
+    struct student **ref;
 
-    struct student *tree[num];
-
-    for (int i = 0; i < sizeof(tree) / sizeof(struct student *); i++)
+    for (i = 0; i < num; i++)
     {
-        tree[i] = malloc(sizeof(struct student));
-        // printf("이름 : ");
-        // scanf(" %s", tree[i]->name);
-        // printf("중간점수 : ");
-        // scanf("%d", &tree[i]->mid);
-        // printf("기말점수 : ");
-        // scanf("%d", &tree[i]->final);
-        printf("이름, 중간점수, 기말점수 \n");
-        scanf("%s %d %d", tree[i]->name, &tree[i]->mid, &tree[i]->final);
+        // while(scanf("%s %d %d", p_name, &tree->mid, &tree->final ) !=EOF && i++ < num)
+        {
+            printf("이름, 중간점수, 기말점수\n");
+            scanf("%s %d %d", p_name, &tree->mid, &tree->final);
+            tree->name = p_name;
 
-        ref = (struct StudentData **)tsearch((void *)tree[i], (void **)&root, compare);
-        printf("%s", (*ref)->name);
-
-        if (*ref == tree[i])
-            printf("추가되었습니다.\n");
-        else
-            printf("이미 존재합니다.\n");
-    }
-    twalk((void *)root, print_node);
-
-    for (int i = 0; i < sizeof(tree) / sizeof(struct StudentData *); i++)
-    {
-        free(tree[i]);
+            ref = (struct student **)tsearch((void *)tree, (void **)&root, compare);
+            printf("%s ", (*(struct student **)ref)->name);
+            if (*ref == tree)
+                printf("= 추가되었습니다.\n");
+            else
+                printf("= 이미 존재합니다.\n");
+            p_name += strlen(p_name) + 1;
+            tree++;
+        }
+        twalk((void *)root, print_node);
     }
 }
-
